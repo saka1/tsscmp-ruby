107c487ff05b1d7c64bcb8096d8170209fdfe60e
saka1 <github@saka1.net>
2019-04-09 12:50:27 +0000

    Update comment

--- lib/tsscmp.rb
+++ lib/tsscmp.rb
@@ -8,7 +8,7 @@ module Tsscmp
 
   # Constant time string comparison.
   #
-  # Returns true if the two arguments are same value, otherwise false.
+  # Returns true if the two arguments have same value, otherwise false.
   def compare(a, b) # rubocop:disable Naming/UncommunicativeMethodParamName:
     return false if a.nil? || b.nil?
 
@@ -19,6 +19,13 @@ module Tsscmp
     key = SecureRandom.random_bytes(32)
     ah = OpenSSL::HMAC.hexdigest('sha256', key, a)
     bh = OpenSSL::HMAC.hexdigest('sha256', key, b)
+
+    # Implementation Note:
+    # `&& a == b` is a very very conservative comparison.
+    # In my opnion, `ah == bh` is enough to compare securely although several other implementations use the strictly method such as:
+    #  - https://github.com/rails/rails/blob/v5.2.3/activesupport/lib/active_support/security_utils.rb#L27
+    #  - https://github.com/suryagh/tsscmp/blob/v1.0.6/lib/index.js#L35
+    # So, as of the present time, I add `&& a == b`.
     ah == bh && a == b
   end
 end
